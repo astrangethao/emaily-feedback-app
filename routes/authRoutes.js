e033436447453697318b994942ae5732d4f5f301
Andrew Nguyen <anguyen1320@gmail.com>
2020-08-04 13:57:18 -0500

    intial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,13 +9,19 @@ module.exports = (app) => {
     })
   );
 
-  //route handler to resend code to google oauth
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  //route handler to resend code to google oauth, and redirect user to dashboard component
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   //handler for logging out
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -26,20 +26,18 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       //check for already existing users
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          //we already have a record with the given profile ID
-          //tells passport that oauth is done
-          done(null, existingUser);
-        } else {
-          //create a new user record
-          new User({ googleId: profile.id }) //creates a new instance of a user on the server
-            .save() //saves user to the database as a record
-            .then((user) => done(null, user)); //tells passport that oauth is done
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        //tells passport that oauth is done
+        return done(null, existingUser);
+      }
+
+      //create a new user record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
