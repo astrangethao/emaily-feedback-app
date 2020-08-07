35721808e8d2c291586fa807d743eaca953944eb
Andrew Nguyen <anguyen1320@gmail.com>
2020-08-07 11:30:49 -0500

    fixing deployment error

---
+++ client/build/precache-manifest.2e36f088ab970ec4f1318e50e29bddd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e61d1c55b912f6cfa8e408a3a32cc85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad84e30294194a18105",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "aad84e30294194a18105",
+    "url": "/static/js/2.267dff26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.267dff26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d64434b8a2bc37ab20",
+    "url": "/static/js/main.bc8646e1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
